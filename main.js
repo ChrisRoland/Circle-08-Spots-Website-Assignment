d74ada1dcfc6109f158b7257cfe51ae8635d4b36
ChrisRoland <chrisebuberoland@gmail.com>
2025-05-25 17:32:13 +0100

    save

--- main.js
+++
@@ -1,7 +0,0 @@
-//function to get the current year for the copyright
-function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-}
-
-document.querySelector('.footertext').textContent = `${getCurrentYear()} © Spots`;
